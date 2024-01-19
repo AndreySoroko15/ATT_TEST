fa347d61340b5017c834a1e5dd33eb12b78e216d
Andrew <games.soroko@mail.ru>
2024-01-19 11:25:56 +0300

    output data from the search array to a table

--- js/search.js
+++ js/search.js
@@ -27,6 +27,7 @@
 
     function getJson(data) {
         $('#search').on('input', function() {
+            let tbody = $('.orders_table tbody');
             // console.log(typeof data);
             jsonToArray = JSON.parse(data);
             // console.log(jsonObj);
@@ -46,6 +47,18 @@
                 }
             })
             // console.log(filteredArray);
+            tbody.empty();
+
+            filteredArray.forEach(function(val) {
+                let tableRow = "<tr>" +
+                    "<td>" + val.order_id + "</td>" +
+                    "<td>" + val.name + "</td>" +
+                    "<td>" + val.title + "</td>" +
+                    "<td>" + val.cost + "</td>" +
+                    "</tr>";
+    
+                tbody.append(tableRow);
+            });
             
         })
     }
